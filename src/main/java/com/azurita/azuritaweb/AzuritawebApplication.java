0ada2bf9064af7a8901fd388809ff8c011a502c7
Carlos Romero <caaromerogi@unal.edu.co>
2022-09-03 16:31:46 -0500

    Start

---
+++ src/main/java/com/azurita/azuritaweb/AzuritawebApplication.java
@@ -0,0 +1,13 @@
+package com.azurita.azuritaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuritawebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuritawebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/azurita/azuritaweb/AzuritawebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azurita.azuritaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuritawebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
